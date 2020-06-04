8de62283cdeaec553dc7cc91cfc8c19a7b5d7167
chayasara <57223241+chayasara@users.noreply.github.com>
2020-06-04 18:08:28 +0300

    home work
    I worked on this home work for many hours, and to my great dismay I  wasn't able to complete it😒.

--- dna_sequence.cpp
+++ dna_sequence.cpp
@@ -4,11 +4,22 @@
 
 void DnaSequence::initField(const char *sequence, bool isOverriding)
 {
+	size_t len = strlen(sequence);
+	DnaChar *d = new DnaChar [len];
+	
+	for(size_t i = 0; i < len; ++i, d[i] = sequence[i]);
+	initField(d, isOverriding);
+	delete [] d;
+}
+
+void DnaSequence::initField(const DnaSequence::DnaChar *sequence, bool isOverriding)
+{
 	checkIfNull(sequence);
 	size_t len = strlen(sequence);
-	char *tmp = new char [len+1];
+	DnaChar *tmp = new DnaChar [sizeof(DnaChar)*(len+1)];
 	
-	strcpy(tmp, sequence);
+	for(int i = 0; i<len; ++i)
+		tmp[i] = sequence[i];
 	
 	if(isOverriding)
 		delete [] m_sequence;
@@ -17,9 +28,35 @@ void DnaSequence::initField(const char *sequence, bool isOverriding)
 	m_sequence_length = len;
 }
 
-void DnaSequence::checkIfNull(const char *sequence)const
+void DnaSequence::checkIfNull(const DnaSequence::DnaChar& *sequence)const
 {
 	if(sequence == NULL)
 		throw (std::invalid_argument("invalid sequence"));
 	
 }
+
+DnaSequence::DnaChar::DnaChar(char c)
+:m_necleotide(c)
+{
+	if(m_necleotide != 'A' && m_necleotide != 'T' && m_necleotide != 'C' && m_necleotide != 'G')
+		throw (std::invalid_argument("Necleotide must be A, T, C or G"));
+}
+
+char DnaSequence::DnaChar::get_char()const
+{
+	return m_necleotide;
+}
+
+DnaChar& operator=(DnaChar rv)
+{
+	m_necleotide = rv.m_necleotide;
+}
+
+bool DnaSequence::strcmp(DnaChar *lv, DnaChar *rv) const
+{
+	for(int i=0; i<m_sequence_length; ++i)
+		if(lv[i].get_necleotide() != rv[i].get_necleotide())
+			return false;
+	return true;
+}
+
--- dna_sequence.h
+++ dna_sequence.h
@@ -8,30 +8,53 @@
 
 class DnaSequence
 {
-	friend std::ostream& operator<<(std::ostream&, DnaSequence&);
-	
+
+friend std::ostream& operator<<(std::ostream&, DnaSequence&);
+
+private:
+	class DnaChar{
+	public:
+		DnaChar(){}
+		DnaChar(char c);
+		DnaChar& operator=(DnaChar rv);
+		char get_necleotide()const;
+		
+		                                                                                                                                                                            
+	private:
+		char m_necleotide;
+	};
 public:
 	DnaSequence(const char *sequence);
 	DnaSequence(const std::string &sequence);
 	DnaSequence(const DnaSequence &other);
 	~DnaSequence();
-	
+
 	DnaSequence& operator=(const DnaSequence &other);
-	DnaSequence& operator=(const std::string &sequence);
-	DnaSequence& operator=(const char* sequence);
 	
 	bool operator==(const DnaSequence &other)const;
 	bool operator!=(const DnaSequence &other)const;
 	
-	char operator[](size_t index)const;
+	DnaChar const operator[](size_t index)const;
+	DnaChar operator[](size_t index);
 	size_t length()const;
 	
+	//char* get_sequence()const;
+	
 private:
-	char *m_sequence;
+	
+	DnaChar *m_sequence;
 	size_t m_sequence_length;
 	
+	
+	
+	void set(size_t index, char value);
+	
+	void initField(const DnaChar* sequence, bool isOverriding = false);
 	void initField(const char* sequence, bool isOverriding = false);
 	void checkIfNull(const char*)const;
+	
+	bool strcmp(DnaChar *, DnaChar *) const;
+	
 };
 
 
@@ -66,38 +89,38 @@ inline DnaSequence& DnaSequence::operator=(const DnaSequence &other)
 	return *this;
 }
 
-inline DnaSequence& DnaSequence::operator=(const std::string &sequence)
-{
-	initField(sequence.c_str(), true);
-}
-
-inline DnaSequence& DnaSequence::operator=(const char* sequence)
-{
-	initField(sequence, true);
-}
-
 inline bool DnaSequence::operator==(const DnaSequence &other)const
 {
-	return (strcmp(m_sequence, other.m_sequence) == 0);
+	return (strcmp(m_sequence, other.m_sequence));
 }
 
 inline bool DnaSequence::operator!=(const DnaSequence &other)const
 {
-	return (strcmp(m_sequence, other.m_sequence) != 0);
+	return (!strcmp(m_sequence, other.m_sequence));
 }
 
 inline std::ostream& operator<<(std::ostream& os, DnaSequence& dna)
 {
-	return os << dna.m_sequence;
+	for(size_t i=0; i<dna.m_sequence_length; ++i, os<<dna.m_sequence[i].get_necleotide());
+	return os;
 }
 
-inline char DnaSequence::operator[](size_t index)const
+
+inline const DnaSequence::DnaChar DnaSequence::operator[](size_t index)const
+{
+	return m_sequence[index];
+}
+
+inline DnaSequence::DnaChar DnaSequence::operator[](size_t index)
 {
 	return m_sequence[index];
 }
 
 inline size_t DnaSequence::length()const
 {
-	return m_sequence_length;;
+	return m_sequence_length;
 }
+
+
+
 #endif
